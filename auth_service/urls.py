cf271d76d99e8f180e4720f147fedf9c95df3b67
haze-me <harrisonaka29@gmail.com>
2025-03-31 02:14:37 +0100

    add rides_book and staff apps

--- auth_service/urls.py
+++ auth_service/urls.py
@@ -5,4 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
+    path('api/rides/', include('ride_booking.urls')),
+    path('api/staff/', include('staffs.urls')),
+
 ]
